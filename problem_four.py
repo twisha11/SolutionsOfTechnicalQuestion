f909750bbfdf5db7292b5d92372a9d279aec38db
Twisha <67854912+twisha11@users.noreply.github.com>
2023-09-17 14:48:42 +0530

    Solution Of Problem4
    Q4.  Swap two Strings Without Using any Third Variable
            Input: a = "Hello", b = "World".
            Output: Strings after swap: a = World and b = Hello

---
+++ problem_four.py
@@ -0,0 +1,17 @@
+# Input strings
+a = "Hello"
+b = "World"
+
+print("Before Swap ")
+print("a =",a)
+print("b= ",b)
+
+print("-------------------------")
+
+# Swapping without using a third variable
+a, b = b, a
+
+# Print the result
+print("After Swap")
+print("a =",a)
+print("b =",b)
