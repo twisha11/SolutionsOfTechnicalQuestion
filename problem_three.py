10c67eeb929dd2266c701f73293003ef13553b14
Twisha <67854912+twisha11@users.noreply.github.com>
2023-09-17 14:47:26 +0530

    Solution Of Problem3
    Q3.  Biggest and smallest number in an array
    --- Write a program to print the biggest and smallest number in the array. 
             Input: arr[] = {1, 2, 3, 4, 5}
             Output: Maximum is: 5
             Minimum is: 1
    
    Input: arr[] = {5, 3, 7, 4, 2}
    Output: Maximum is: 7
    Minimum is: 2

---
+++ problem_three.py
@@ -0,0 +1,19 @@
+# Example 1
+arr1 = [1, 2, 3, 4, 5]
+max1 = max(arr1)
+min1 = min(arr1)
+print("Input :",arr1)
+print("Output : ")
+print("       Maximum is:",max1)
+print("       Minimum is:",min1)
+
+print("------------------------")
+# Example 2
+arr2 = [5, 3, 7, 4, 2]
+max2 = max(arr2)
+min2 = min(arr2)
+print("Input :",arr2)
+print("Output : ")
+print("       Maximum is:",max2)
+print("       Minimum is:",min2)
+
